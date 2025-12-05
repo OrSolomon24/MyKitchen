4612adaaeccf7036c426df9be4163491f7d96c11
Or Solomon <orsolomon24@gmail.com>
2025-12-05 12:42:56 +0200

    sync

--- server/index.js
+++ server/index.js
@@ -46,8 +46,8 @@ module.exports = app;
 
 
 // comment out when developing locally to run the backend on port 5000
-const PORT = process.env.PORT || 5000;
+// const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
-});
+// app.listen(PORT, () => {
+//   console.log("Server running on port", PORT);
+// });
